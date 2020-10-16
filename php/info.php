727babed40b8f3aaf37f13be1020301381d9663d
BROCHET FRANCK <GU1034@nova.infra-grdf.com>
2020-10-16 15:53:31 +0200

    Update info.php
    Correction sur notice

--- php/info.php
+++ php/info.php
@@ -8,9 +8,9 @@ try
 //custom configuration
     $obj = ini_get_all();
     echo json_encode(array(
-        'upload_max_filesize' => $obj['upload_max_filesize']['global_value']*1000000,
-        'max_file_uploads' => $obj['max_file_uploads']['global_value']*1,        
-        'post_max_size' => $obj['post_max_size']['global_value']*1000000,
+        'upload_max_filesize' => intval($obj['upload_max_filesize']['global_value']) * 1000000,
+        'max_file_uploads' => intval($obj['max_file_uploads']['global_value']) * 1,        
+        'post_max_size' => intval($obj['post_max_size']['global_value']) * 1000000,
         'tag_swallow' => time()
     ));
 } catch (Exception $e) {
